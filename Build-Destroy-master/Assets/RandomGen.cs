9fb750c9e71e17d3d7bce27ea551d075bdbf5a84
MOStLaurentCyr <mcyx03@gmail.com>
2018-03-03 03:46:26 -0500

    Added Gen for radomized cubes

---
+++ Build-Destroy-master/Assets/RandomGen.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomGen {
+    private int min;
+    private int max;
+
+    // Use this for initialization
+    public RandomGen (int min, int max) {
+        this.min = min;
+        this.max = max;
+    }
+
+    public int GetNbr()
+    {
+        UnityEngine.Random.InitState(Time.frameCount);
+        return UnityEngine.Random.Range(min, max++);
+    }
+}
